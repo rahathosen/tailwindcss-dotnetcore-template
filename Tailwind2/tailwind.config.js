ff313ddfa7454460dd2668e4b2324a7bb57e0e97
rahathosen <70264343+rahathosen@users.noreply.github.com>
2021-07-05 12:56:02 +0600

    up init

---
+++ Tailwind2/tailwind.config.js
@@ -0,0 +1,23 @@
+﻿const colors = require('tailwindcss/colors')
+
+module.exports = {
+   
+    purge: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.chstml'
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                cyan: colors.cyan
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('tailwindcss-textshadow')
+    ]
+}
